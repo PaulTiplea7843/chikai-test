cbbc56f03fe25caf6091e9a23b429544dcc846dd
PaulTiplea7843 <paul.tiplea2018@gmail.com>
2022-02-28 16:11:54 +0200

    sql config

--- server/db.sql
+++ server/db.sql
@@ -1,3 +1,11 @@
+
+/* These are the steps you need to follow for the db configuration:
+1. Open terminal and run  sudo -u postgres psql 
+2. After that run these commands: - create user 'postgres' with encrypted password 'asdQWE123';
+                                  - grant all privileges on database contacts to postgres;
+3. After that  run all these command bellow this comment one at a time.                              
+*/
+
 CREATE TABLE contacts;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
